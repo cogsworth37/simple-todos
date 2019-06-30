83d1a77c5a1cb3491c1e33ea8c9485acb521a936
Andy <andy@Andys-MacBook-Pro.local>
2019-06-30 17:14:04 -0500

    Initial Commit

---
+++ client/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="task in tasks">
+        {{task}} 
+        <button @click="tasks.splice(tasks.indexOf(task), 1)"> Remove Task </button>
+      </li>
+    </ul>
+    <input v-model="new_task" placeholder="Enter Task...">
+    <button @click="add_task(tasks, new_task)">
+      Add Task
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: ["Task 1", "Task 2", "Task 3"],
+      new_task: ""
+    };
+  },
+  methods: {
+    add_task(tasks, new_task, event) {
+      console.log(tasks);
+      console.log(new_task);
+      tasks.push(new_task)
+      this.new_task = ""
+    },
+  }
+}
+</script>
+
+<style scoped>
+  p {
+    font-size: 2em;
+    text-align: center;
+  }
+</style>
